882d39f6da2f1cb46b44db85a66df34d002ca889
ffantasy <qwertxp@gmail.com>
2017-04-07 17:52:57 +0800

    Update tis.sh
    add option to decide whether WOKER container can be run on controler host or not.

--- tis.sh
+++ tis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
-echo "USAGE: $0 TsungConfigFile ClientAmount"
-echo "e.g.: $0 config.xml 1"
+if [ $# != 3 ]; then
+echo "USAGE: $0 TsungConfigFile ClientAmount CanClientRunOnControler"
+echo "e.g.: $0 config.xml 1 0"
 exit 1;
 fi
 
-IMAGE_NAME=ffantasy/tsung-in-swarm:17011601
+IMAGE_NAME=ffantasy/tsung-in-swarm:17040701
 PREFIX=ff-tsung-
 NETWORK_NAME=${PREFIX}network
 CONTROLER_NAME=${PREFIX}controler
@@ -16,7 +16,7 @@ echo -e "\e[33mcreate tsung network...\e[0m"
 docker network create -d overlay $NETWORK_NAME
 
 echo -e "\e[33mcreate tsung controler...\e[0m"
-docker service create --name $CONTROLER_NAME --replicas=1 -p8091:8091 --network $NETWORK_NAME --mount type=bind,source=$1,destination=/tmp/config.xml $IMAGE_NAME sleep infinity
+docker service create --name $CONTROLER_NAME --constraint 'node.role == manager' --replicas=1 -p8091:8091 --network $NETWORK_NAME --mount type=bind,source=$1,destination=/tmp/config.xml $IMAGE_NAME sleep infinity
 controlerId=''
 while [ -z $controlerId ]; do
     sleep 1s
@@ -24,7 +24,11 @@ while [ -z $controlerId ]; do
 done
 
 echo -e "\e[33mcreate tsung clients...\e[0m"
-docker service create --name $CLIENT_NAME --replicas=$2 --network $NETWORK_NAME $IMAGE_NAME bash runclient.sh $CONTROLER_NAME
+CONSTRAINT=""
+if [ $3 -eq 0 ]; then
+    CONSTRAINT="--constraint 'node.role != manager'"
+fi
+eval docker service create --name $CLIENT_NAME $CONSTRAINT --replicas=$2 --network $NETWORK_NAME $IMAGE_NAME bash runclient.sh $CONTROLER_NAME
 
 echo -e "\e[33mrun contorler...\e[0m"
 docker exec -it $controlerId bash runcontroler.sh $2
