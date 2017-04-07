e61dc8683ab336d471463a9027f2223d4f9a039a
ffantasy <qwertxp@gmail.com>
2017-04-07 17:55:25 +0800

    Update runclient.sh
    fix hostname issue.

--- runclient.sh
+++ runclient.sh
@@ -5,6 +5,8 @@ echo "usage error."
 exit 1;
 fi
 
+IP=$(hostname -i | sed "s/ .*//")
+HOST=$(echo $IP | sed "s/\./-/g; s/^/worker-/")
 service ssh start
-ssh $1 "echo '<monitor host=\"$(hostname)\" />' >> /root/monitors.txt && echo '<client host=\"$(hostname)\" />' >> /root/clients.txt"
+ssh $1 "echo '<monitor host=\"${HOST}\" />' >> /root/monitors.txt && echo '<client host=\"${HOST}\" />' >> /root/clients.txt && echo ${IP}' '${HOST} >> /etc/hosts"
 sleep infinity
