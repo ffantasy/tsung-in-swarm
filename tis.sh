81f031e1e1b773d242433dbdac385cd58a7745eb
ffantasy <qwertxp@gmail.com>
2017-12-27 17:48:59 +0800

    Update tis.sh

--- tis.sh
+++ tis.sh
@@ -16,7 +16,7 @@ echo -e "\e[33mcreate tsung network...\e[0m"
 docker network create -d overlay $NETWORK_NAME
 
 echo -e "\e[33mcreate tsung controler...\e[0m"
-docker service create --name $CONTROLER_NAME --constraint 'node.role == manager' --replicas=1 -p8091:8091 --network $NETWORK_NAME --mount type=bind,source=$1,destination=/tmp/config.xml $IMAGE_NAME sleep 3650000d
+docker service create -d --name $CONTROLER_NAME --constraint 'node.role == manager' --replicas=1 -p8091:8091 --network $NETWORK_NAME --mount type=bind,source=$1,destination=/tmp/config.xml $IMAGE_NAME sleep 3650000d
 controlerId=''
 while [ -z $controlerId ]; do
     sleep 1s
@@ -28,7 +28,7 @@ CONSTRAINT=""
 if [ $3 -eq 0 ]; then
     CONSTRAINT="--constraint 'node.role != manager'"
 fi
-eval docker service create --name $CLIENT_NAME $CONSTRAINT --replicas=$2 --network $NETWORK_NAME $IMAGE_NAME bash runclient.sh $CONTROLER_NAME
+eval docker service create -d --name $CLIENT_NAME $CONSTRAINT --replicas=$2 --network $NETWORK_NAME $IMAGE_NAME bash runclient.sh $CONTROLER_NAME
 
 echo -e "\e[33mrun contorler...\e[0m"
 docker exec -it $controlerId bash runcontroler.sh $2
@@ -37,3 +37,4 @@ echo -e "\e[33mremove service...\e[0m"
 docker service rm $CLIENT_NAME $CONTROLER_NAME
 #make sure remove controler container
 docker rm -f $(docker ps -aqf "name=$CONTROLER_NAME") > /dev/null
+
