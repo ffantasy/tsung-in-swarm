aba1e41c43b8af20a92699ca5c0a9fbfe6402b43
ffantasy <qwertxp@gmail.com>
2019-10-09 11:36:46 +0800

    fix bug

--- runclient.sh
+++ runclient.sh
@@ -8,5 +8,5 @@ fi
 IP=$(hostname -i | sed "s/ .*//")
 HOST=$(echo $IP | sed "s/\./-/g; s/^/worker-/")
 /usr/sbin/sshd
-ssh $1 "echo '<monitor host=\"${HOST}\" />' >> /root/monitors.txt && echo '<client host=\"${HOST}\" />' >> /root/clients.txt && echo ${IP}' '${HOST} >> /etc/hosts"
+ssh $1 "echo '<monitor host=\"${HOST}\" \/>' >> /root/monitors.txt && echo '<client host=\"${HOST}\" \/>' >> /root/clients.txt && echo ${IP}' '${HOST} >> /etc/hosts"
 sleep 3650000d
